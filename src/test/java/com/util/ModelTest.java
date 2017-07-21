be95bea79b825f9a3cc985e0d6ec7a6fe415994b
bogdan olexandr <bogdanolexandr@gmail.com>
2017-07-21 20:15:31 +0300

    add ModelTest

---
+++ src/test/java/com/util/ModelTest.java
@@ -0,0 +1,18 @@
+package com.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ModelTest {
+
+	private Model model = new Model();
+	
+	@Test
+	public void testAddAttribute() {
+		model.addAttribute("attr", "value");
+		String expect = "value";
+		assertEquals(expect,model.getAttribute("attr"));
+	}
+
+}
