0a7e18c48c3b17d97fcc9cd06871820260ae647d
bogdan olexandr <bogdanolexandr@gmail.com>
2017-07-23 20:00:43 +0300

    add tests

--- src/test/java/com/service/GameTest.java
+++ src/test/java/com/service/GameTest.java
@@ -3,6 +3,8 @@ package com.service;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import com.exception.WrongRangeException;
+
 public class GameTest {
 
 	private Game game = new Game(1, 15);
@@ -15,12 +17,37 @@ public class GameTest {
 		assertEquals(expectedMax, game.getMaxVal());
 	}
 
+	@Test(expected = WrongRangeException.class)
+	public void testCreateGameWrongMinParam() {
+		Game game = new Game(0,15);
+	}
+	
+	@Test(expected = WrongRangeException.class)
+	public void testCreateGameWrongMaxParam() {
+		Game game = new Game(1,100);
+	}
+	
+	@Test(expected = WrongRangeException.class)
+	public void testCreateGameWrongBothParams() {
+		Game game = new Game(0,100);
+	}
+	
 	@Test
-	public void testGuessWrongInput() {
+	public void testGuessWrongMinRangeInput() {
 		game.guess(0);
 		assertEquals(false, game.isCorrectEnter());
+	}
+	
+	@Test
+	public void testGuessWrongMaxRangeInput() {
 		game.guess(16);
 		assertEquals(false, game.isCorrectEnter());
+	}
+	
+	@Test
+	public void testGuessRightParam() {
+		game.guess(5);
+		assertEquals(true, game.isCorrectEnter());
 	}
 
 }
